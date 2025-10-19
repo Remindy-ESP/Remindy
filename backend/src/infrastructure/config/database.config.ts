14afef5e82a90d02e659d5d85b1317845ba75ff3
Rabieh SASSI <sassi.rabieh@gmail.com>
2025-10-19 18:23:22 +0200

    fix: pipeline and eslint checks for the pipeline

--- backend/src/infrastructure/config/database.config.ts
+++ backend/src/infrastructure/config/database.config.ts
@@ -2,22 +2,22 @@ import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
-        const isDevelopment = configService.get('NODE_ENV') === 'development';
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+    const isDevelopment = configService.get('NODE_ENV') === 'development';
 
-        return {
-            type: 'postgres',
-            url: configService.get('NEON_DATABASE_URL_PROD'),
-            ssl: true,
-            autoLoadEntities: true,
-            synchronize: isDevelopment,
-            logging: isDevelopment,
-            retryAttempts: 3,
-            retryDelay: 3000,
-            migrations: ['dist/infrastructure/database/migrations/*.js'],
-            migrationsRun: !isDevelopment
-        };
-    },
+    return {
+      type: 'postgres',
+      url: configService.get('NEON_DATABASE_URL_PROD'),
+      ssl: true,
+      autoLoadEntities: true,
+      synchronize: isDevelopment,
+      logging: isDevelopment,
+      retryAttempts: 3,
+      retryDelay: 3000,
+      migrations: ['dist/infrastructure/database/migrations/*.js'],
+      migrationsRun: !isDevelopment,
+    };
+  },
 };
--- backend/src/infrastructure/config/typeorm.config.ts
+++ backend/src/infrastructure/config/typeorm.config.ts
@@ -4,11 +4,11 @@ import { config } from 'dotenv';
 config({ path: '.env.production' });
 
 export const AppDataSource = new DataSource({
-    type: 'postgres',
-    url: process.env.NEON_DATABASE_URL_PROD,
-    ssl: true,
-    entities: ['src/infrastructure/database/entities/*.entity.ts'],
-    migrations: ['src/infrastructure/database/migrations/*.ts'],
-    synchronize: false,
-    logging: true,
+  type: 'postgres',
+  url: process.env.NEON_DATABASE_URL_PROD,
+  ssl: true,
+  entities: ['src/infrastructure/database/entities/*.entity.ts'],
+  migrations: ['src/infrastructure/database/migrations/*.ts'],
+  synchronize: false,
+  logging: true,
 });
