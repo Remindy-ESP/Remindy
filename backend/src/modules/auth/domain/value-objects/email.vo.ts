5ed3a20cc9df8c225b039ad4c165e4f8a3980e4e
Irfann95 <irfann.assana@epitech.eu>
2025-12-05 16:05:00 +0100

    SonarQube fixing REGEX

--- backend/src/modules/auth/domain/value-objects/email.vo.ts
+++ backend/src/modules/auth/domain/value-objects/email.vo.ts
@@ -2,13 +2,16 @@ export class Email {
   private readonly value: string;
 
   constructor(value: string) {
-    if (!/^[^@]+@[^@]+\.[^@]+$/.test(value)) {
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+    if (!emailRegex.test(value)) {
       throw new Error('Invalid email format');
     }
+
     this.value = value.toLowerCase();
   }
 
-  getValue() {
+  getValue(): string {
     return this.value;
   }
 }
