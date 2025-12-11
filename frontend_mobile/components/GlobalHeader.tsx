ef24cce1d95e3e60250f149b579b765803a85a2a
Ymoine <yann.moine@epitech.eu>
2025-12-11 14:49:26 +0100

    feature/Calendar Delete unusued var

--- frontend_mobile/components/GlobalHeader.tsx
+++ frontend_mobile/components/GlobalHeader.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Platform, Dimensions, TouchableOpacity, Image } from 'react-native';
+import { View, StyleSheet, Dimensions, TouchableOpacity, Image } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 import BurgerMenu from './BurgerMenu';
