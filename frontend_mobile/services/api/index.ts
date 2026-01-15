283982c02de0d8c813091320feb7a38bb405c59c
Tamen Paternus <85977527+tamen23@users.noreply.github.com>
2026-01-15 22:31:54 +0100

    feat(api): add folder and storage services

--- frontend_mobile/services/api/index.ts
+++ frontend_mobile/services/api/index.ts
@@ -14,6 +14,8 @@ export { default as subscriptionService } from './subscription.service';
 export { default as userService } from './user.service';
 export { default as eventService } from './event.service';
 export { default as documentService } from './document.service';
+export { default as folderService } from './folder.service';
+export { default as storageService } from './storage.service';
 
 // Export types
 export * from './types';
