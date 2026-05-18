01c99b2c183055f92220a130ee7467f31035b3a6
SKALSKI Alan <AlanSkalski@outlook.fr>
2026-05-19 00:19:19 +0200

    chore: remove temporary test scripts

--- backend/run-tests.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-  const output = execSync('npm run test src/modules/scheduler/tasks/process-renewal-notifications.task.spec.ts', { encoding: 'utf-8', cwd: __dirname });
-  console.log(output);
-} catch (e) {
-  console.log(e.stdout);
-  console.log(e.stderr);
-}
